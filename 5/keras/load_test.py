278923cbece5e34107c711bcc43c5526d8a2eec9
= <=>
2022-03-12 19:35:07 +0900

    add csv load tester

--- 5/keras/load_test.py
+++ 5/keras/load_test.py
@@ -1,11 +1,6 @@
 import csv
 
-csv_file = open("./press_log_100Hz/press_logs_20220309-103001_2_checked.csv", "r", encoding="utf-8", errors="", newline="" )
-f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
-
-header = next(f)
-#print(header)
-for row in f:
-    #rowはList
-    #row[0]で必要な項目を取得することができる
-    print(row)
+with open('./press_log_100Hz/press_logs_20220309-102401_2_checked.csv') as f:
+    reader = csv.reader(f)
+    l = [row for row in reader]
+print(len(l))
